8db9357aed4a5bfdb033b2907f48423f635686a1
Jordan Parker <letsgetjordan@gmail.com>
2018-09-10 20:35:29 +0100

    Interface and tesla move into library directory

---
+++ lib/shopiexrl/library/interface.ex
@@ -0,0 +1,33 @@
+defmodule ShopiexRL.Interface do
+  require Logger
+
+  @spec ask(atom()) :: any()
+  def ask(name) do
+    case Process.whereis(name) do
+      nil ->
+        {:error, :not_found}
+
+      pid ->
+        GenServer.call(pid, :health_check)
+    end
+  end
+
+  @spec install(atom()) :: any()
+  def install(name) do
+    Logger.info(fn ->
+      "[ShopiexRL.Interface]: Asking supervisor to start a store for #{inspect(name)}"
+    end)
+    ShopiexRL.Supervisor.add_store(name, 0)
+  end
+
+  @spec increment(atom()) :: any()
+  def increment(name) do
+    case __MODULE__.ask(name) do
+      :ok ->
+        GenServer.call(String.to_atom(name), :increment)
+
+      {:error, :not_found} ->
+        {:error, :not_found}
+    end
+  end
+end
---
+++ lib/shopiexrl/library/tesla.ex
@@ -0,0 +1,58 @@
+defmodule ShopiexRL.Tesla.Middleware do
+  @moduledoc """
+  Middleware between the Tesla client and the Leakybucket process
+  """
+
+  @behaviour Tesla.Middleware
+  require Logger
+
+  def call(env, next, opts) do
+    opts = opts || []
+    name = opts
+
+    case ShopiexRL.Interface.ask(name) do
+      :ok ->
+        Logger.info(fn ->
+          "[API.Tesla.Middleware.Leakybucket]: #{inspect(name)}: ok"
+        end)
+
+        run(env, next, name)
+
+      :backoff ->
+        Logger.info(fn ->
+          "[API.Tesla.Middleware.Leakybucket]: #{inspect(name)}: backoff"
+        end)
+
+        {:error, :backoff}
+
+      {:error, :not_found} ->
+        Logger.info(fn ->
+          "[API.Tesla.Middleware.Leakybucket]: #{inspect(name)} was not found"
+        end)
+
+        ShopiexRL.Interface.install(name)
+        call(env, next, name)
+    end
+  end
+
+  defp run(env, next, name: name) do
+    case Tesla.run(env, next) do
+      {:ok, env} ->
+        Logger.info(fn ->
+          "[API.Tesla.Middleware.Leakybucket]: #{inspect(name)} incrementing value"
+        end)
+
+        ShopiexRL.Interface.increment(name)
+        {:ok, env}
+
+      {:error, reason} ->
+        Logger.info(fn ->
+          "[API.Tesla.Middleware.Leakybucket]: #{inspect(name)} errored with reason #{
+            inspect(reason)
+          }"
+        end)
+
+        {:error, :unavailable}
+    end
+  end
+end
