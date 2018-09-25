cb1480096188c332fc72baaa865a66f2c7c481ac
Jordan Parker <letsgetjordan@gmail.com>
2018-09-25 19:18:10 +0100

    Implements new connection functions

--- lib/shopiexrl/state/connection.ex
+++ lib/shopiexrl/state/connection.ex
@@ -5,7 +5,7 @@ defmodule Shopiexrl.Connection do
     type: :ordered_set,
     autoincrement: true
 
-  def create(pool, pid) when is_atom(pool) and is_pid(pid) do
+  def create(pool \\ :unassigned, pid \\ nil) when is_atom(pool) do
     %__MODULE__{
       id: UUID.uuid4(),
       pool: pool,
@@ -15,8 +15,47 @@ defmodule Shopiexrl.Connection do
     }
   end
 
+  def change_pool(:"$end_of_table", _pool), do: :"$end_of_table"
   def change_pool(self, pool) do
     %__MODULE__{self | pool: pool, updated_at: :os.system_time(:seconds)}
-    |> __MODULE__.write
   end
+
+  def release_lock(:"$end_of_table"), do: :"$end_of_table"
+  def release_lock([self]) do
+    %__MODULE__{self | pid: nil}
+  end
+
+  def assign_to(:"$end_of_table", _pid), do: :"$end_of_table"
+  def assign_to([self], pid) do
+    %__MODULE__{self | pid: pid, updated_at: :os.system_time(:seconds)}
+  end
+
+  ##  ==================================
+  ##  guards
+  ##  ==================================
+  def with_id(id), do: {:==, :id, id}
+
+  def from_pool(pool), do: {:==, :pool, pool}
+  def from_pid(pid), do: {:==, :pid, pid}
+
+  def updated_before(timestamp), do: {:<=, :updated_at, timestamp}
+  def updated_after(timestamp), do: {:>=, :updated_at, timestamp}
+
+  def created_before(timestamp), do: {:<=, :created_at, timestamp}
+  def created_after(timestamp), do: {:>=, :created_at, timestamp}
+
+
+  ##  ==================================
+  ##  Query
+  ##  ==================================
+  def get(guards, opts \\ [limit: 1]), do: Memento.Query.select(__MODULE__, guards, opts)
+
+  def get_unassigned(), do: __MODULE__.get(from_pool(:unassigned))
+
+  def get_assigned(), do: __MODULE__.get(from_pool(:assigned))
+  def get_assigned({:pid, pid}), do: __MODULE__.get([from_pool(:assigned), from_pid(pid)])
+  def get_assigned({:id, id}), do: __MODULE__.get([from_pool(:assigned), with_id(id)])
+
+  def get_cooldown(), do: __MODULE__.get(from_pool(:cooldown))
+
 end
