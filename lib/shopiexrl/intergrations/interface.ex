56af182a635ee5092297f27f46f87825f067b76a
Jordan Parker <letsgetjordan@gmail.com>
2018-09-25 19:17:47 +0100

    Implements new interface functions

--- lib/shopiexrl/intergrations/interface.ex
+++ lib/shopiexrl/intergrations/interface.ex
@@ -8,25 +8,27 @@ defmodule ShopiexRL.Interface do
         {:error, :not_found}
 
       pid ->
-        GenServer.call(pid, :health_check)
+        :ok
     end
   end
 
   @spec install(atom()) :: any()
   def install(name) do
-    ShopiexRL.event(:shopiexrl_store_events, %{
-      name: name,
-      type: :install
-    })
-
-    ShopiexRL.StoreSupervisor.add_store(name, 0)
+    ShopiexRL.event(:shopiexrl_store_events, %{name: name, type: :install})
+    ShopiexRL.StoreSupervisor.add_store(name)
   end
 
-  @spec increment(atom()) :: any()
-  def increment(name) do
-    case __MODULE__.ask(name) do
+  @spec aqu_lock(atom()) :: any()
+  def aqu_lock(name), do: do_call(name, :request_lock)
+
+  @spec release_lock(atom()) :: any()
+  def release_lock(name), do: do_call(name, :release_lock)
+
+  defp do_call(name, action) when is_binary(name), do: String.to_atom(name) |> do_call(action)
+  defp do_call(name, action) do
+    case ask(name) do
       :ok ->
-        GenServer.call(String.to_atom(name), :request_lock)
+        GenServer.call(name, action)
 
       {:error, :not_found} ->
         {:error, :not_found}
