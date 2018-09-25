6392fcca6886b664e94f005ccd0d27c8bfcdf58b
Jordan Parker <letsgetjordan@gmail.com>
2018-09-25 19:17:03 +0100

    Add ID, remove count

--- lib/shopiexrl/state/store.ex
+++ lib/shopiexrl/state/store.ex
@@ -1,9 +1,9 @@
 defmodule State.Store do
   @type t :: %__MODULE__{
+          id: <<_::288>>,
           name: String.t(),
-          count: integer(),
           rates: State.StoreRates.t(),
           killdown: integer()
         }
-  defstruct [:name, :count, :rates, :killdown]
+  defstruct [:id, :name, :rates, :killdown]
 end
